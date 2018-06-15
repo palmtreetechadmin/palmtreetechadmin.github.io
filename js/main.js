3fe50e5aa88f4b1bd93e7bff0871ac163c9e7c2a
Rajesh Koilpillai <rajesh.koilpillai@gmail.com>
2018-06-15 11:49:47 +0530

    modified counter date

--- js/main.js
+++ js/main.js
@@ -56,7 +56,7 @@ $(window).load(function(){
 
 
 $('#countdown').countdown({
-	date: "December 14, 2019 18:03:26",
+	date: "Jul 15, 2018 18:03:26",
 	render: function(data) {
 	  var el = $(this.el);
 	  el.empty()
